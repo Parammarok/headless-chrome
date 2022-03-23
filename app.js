0896fb1e280945c8ffd190c01ae7195e8068c519
ParaMMarok <rockingboy3600@gmail.com>
2022-03-23 19:52:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.get('/', function(req, res) {
 
             const page = await browser.newPage();
             await page.goto(urlToScreenshot, { waitUntil: 'networkidle2' });
+            await delay(10000);
              await page.screenshot().then(function(buffer) {
                 res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
                 res.setHeader('Content-Type', 'image/png');
