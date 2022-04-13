2522f03b53bfc3ecbf945eee2ee17fe635ea18d4
ParaMMarok <rockingboy3600@gmail.com>
2022-04-13 16:28:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.get('/', function(req, res) {
             height: 800
                 });   
             await Promise.all([ await page.click("#F1 > button") ]);
+            await Promise.all([ await page.click("#F1 > button") ]);
             await page.waitForNavigation({waitUntil: 'networkidle2'});
             const html = await page.content();
             console.log(html);
