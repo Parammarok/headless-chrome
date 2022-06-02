10051173f45dca8ceb822be0defbbcbbd1dedf80
ParaMMarok <rockingboy3600@gmail.com>
2022-06-02 06:08:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,41 +13,52 @@ var parseUrl = function(url) {
     return url;
 };
 
-
-function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
-
 app.get('/', function(req, res) {
     var urlToScreenshot = parseUrl(req.query.url);
-
+     var mode = req.query.mode;
     if (validUrl.isWebUri(urlToScreenshot)) {
         console.log('Screenshotting: ' + urlToScreenshot);
         (async() => {
             const browser = await puppeteer.launch({
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
             });
+
             const page = await browser.newPage();
-            await page.goto(urlToScreenshot, {waitUntil: 'networkidle2'});
-            await page.setViewport({
-            width: 1200,
-            height: 800
-                });   
-            await timeout(1000);
-            
-           await page.evaluate(() => {
-  [...document.querySelectorAll('.elements button')].find(element => element.textContent === 'Download Video').click();
-});
-           
-            await page.waitForNavigation({waitUntil: 'networkidle2'});
-            await page.screenshot().then(function(buffer) {
-                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
-                res.setHeader('Content-Type', 'image/png');
-                res.send(buffer)
-            });
-            const html = await page.content();
+            await page.goto(urlToScreenshot);
+			
+			await page.goto(urlToScreenshot, {waitUntil: 'networkidle2'});
+			
+			
+			if ( mode == 'res')  {
+          const html = await page.content();
             console.log(html);
-            //res.send(html);
+           res.send(html); } 
+            
+            await browser.close();
+		   } 
+			
+			
+          
+			if ( mode == 's')  {
+           await page.screenshot().then(function(buffer) {
+                res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+              res.setHeader('Content-Type', 'image/png');
+               res.send(buffer)		  		   
+           }); 
+		     const html = await page.content();
+            console.log(html);
+		   } else {
+			await page.evaluate(() => document.querySelector("#F1 > button").click());
+		   await page.$x('//*[@id="F1"]/button')
+		   const elements = await page.$x('//*[@id="F1"]/button')
+           await elements[0].click() 
+
+			
+			await page.waitForNavigation({waitUntil: 'networkidle2'});
+             const html = await page.content();
+            console.log(html);
+           res.send(html); } 
+            
             await browser.close();
         })();
     } else {
